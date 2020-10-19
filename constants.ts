fb813659f0aa8c367d4a31382cf4bf8381957797
treets <mail@andreas-theis.net>
2020-10-19 10:35:24 +0200

    Update pxt.json, constants.ts, motion-config.json, pins-config.json, motion.ts, pins.ts

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 namespace project {
-    export const HEADER_ID = '92b3bac5-1f66-4cf8-5829-e4674726d1b2';
+    export const HEADER_ID = 'b973fd6a-b30a-4195-b754-8a2204252de7';
 }
---
+++ motion.ts
@@ -0,0 +1,5 @@
+namespace motion {
+	//% fixedInstance whenUsed
+	export const axis0 = motion.factory.createAxis('axis0', {"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0}, undefined);
+
+}
