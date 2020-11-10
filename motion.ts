9790990dda05b34d955661b9422ad01f7ea6efae
treets <mail@andreas-theis.net>
2020-11-10 12:59:33 +0100

    Update and rename motion.ts to motion-instances.ts

--- motion.ts
+++
@@ -1,8 +0,0 @@
-namespace motion {
-	//% fixedInstance whenUsed
-	export const axis1 = motion.factory.createAxis('axis1', {"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0}, undefined);
-	//% fixedInstance whenUsed
-	export const axis2 = motion.factory.createAxis('axis2', {"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0}, undefined);
-	//% fixedInstance whenUsed
-	export const axis3 = motion.factory.createAxis('axis3', {"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0}, undefined);
-}
