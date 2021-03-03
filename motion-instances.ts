403fec8bbff6336a31c33c8341b0c387bf9d7875
treets <mail@andreas-theis.net>
2021-03-03 14:43:52 +0100

    Update pxt.json, motion-instances.ts, toolbox.xml

--- motion-instances.ts
+++ motion-instances.ts
@@ -1,5 +1,5 @@
 namespace motion {
-	export const config: any = {"axes":[{"limits":{"position":{"min":0,"max":0},"velocity":{"min":0,"max":0},"acceleration":{"min":0,"max":0},"torque":{"min":0,"max":0}},"axisParams":{"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0},"id":"axis1","address":"axis1"},{"limits":{"position":{"min":0,"max":0},"velocity":{"min":0,"max":0},"acceleration":{"min":0,"max":0},"torque":{"min":0,"max":0}},"axisParams":{"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0},"id":"axis2","address":"axis2"}],"kins":[],"points":[]};
+	config = {"axes":[{"limits":{"position":{"min":0,"max":0},"velocity":{"min":0,"max":0},"acceleration":{"min":0,"max":0},"torque":{"min":0,"max":0}},"axisParams":{"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0},"id":"axis1","address":"axis1"},{"limits":{"position":{"min":0,"max":0},"velocity":{"min":0,"max":0},"acceleration":{"min":0,"max":0},"torque":{"min":0,"max":0}},"axisParams":{"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0},"id":"axis2","address":"axis2"}],"kins":[],"points":[]};
 	//% fixedInstance whenUsed
 	export const axis1 = motion.factory.createAxis('axis1', {"x":0,"y":0,"z":0,"xr":0,"yr":0,"zr":0}, undefined);
 	//% fixedInstance whenUsed
